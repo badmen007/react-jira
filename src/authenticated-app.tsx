de1743f70d56884ad49b9fd9b770dfb644d6ab79
badman007 <1572226525@qq.com>
2022-02-02 23:14:13 +0800

    chore: 处理一些警告

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -21,16 +21,17 @@ export const AuthenticatedApp = () => {
             overlay={
               <Menu>
                 <Menu.Item>
-                  <a onClick={logout}>登出</a>
+                  <Button type={"link"} onClick={logout}>
+                    登出
+                  </Button>
                 </Menu.Item>
               </Menu>
             }
           >
-            <a onClick={(e) => e.preventDefault()}>
-              {" "}
+            <Button type={"link"} onClick={(e) => e.preventDefault()}>
               {/* 默认显示 */}
               Hi, {user?.name}
-            </a>
+            </Button>
           </Dropdown>
         </HeaderRight>
       </Header>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,16 +4,23 @@ import { useEffect, useState } from "react";
 export const isFalsy = (value: unknown): boolean =>
   value === 0 ? false : !value;
 
-export const cleanObject = (object: object) => {
+export const isVoid = (value: unknown): boolean =>
+  value === undefined || value === null || value === "";
+
+export const cleanObject = (object: { [key: string]: unknown }) => {
   // 不要污染传入的对象
   //Object.assign({}, object) 相当于
+  /*let a: object; 
+  a = {name: 'xz'}
+  a = new RegExp('');
+  a = () => {}
+  可以赋很多类型的值 并不是我们想要的 键值对  */
+
   const result = { ...object };
 
   Object.keys(result).forEach((key) => {
-    //@ts-ignore
     const value = result[key];
-    if (isFalsy(value)) {
-      //@ts-ignore
+    if (isVoid(value)) {
       delete result[key];
     }
   });
@@ -24,6 +31,7 @@ export const useMount = (callback: () => void) => {
   // 这个主要是为了  页面刷新就执行一次
   useEffect(() => {
     callback();
+    // TODO 依赖项中放入callback的话 会造成无限循环的现象
   }, []);
 };
 
