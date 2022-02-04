1810832e72c5b116e80114b343a740f2b4b7c729
badman007 <1572226525@qq.com>
2022-02-04 16:45:23 +0800

    feat: useSearchParam和useMemo解决循环问题和iterator

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -6,20 +6,26 @@ import styled from "@emotion/styled";
 import { Typography } from "antd";
 import { useProjects } from "utils/project";
 import { useUsers } from "utils/user";
+import { useUrlQueryParam } from "utils/url";
 
 //js本身是弱类型的语言 很多的错误实在 runtime 的时候发现的  但是我们希望在写代码的时候就能发现错误 这就要用到ts
 //ts是强类型的js, 换句话说就是给js增加其它强类型语言那样的类型约束 但是最终ts会被编译成es5
 
 const ProjectListScreen = () => {
-  const [param, setParam] = useState({ name: "", personId: "" });
+  // const [keys] = useState<('name'|'personId')[]>(['name', 'personId']) // 居然还能这么写
+  //基本类型可以放到依赖中 组件的状态可以放到依赖中 非组件状态的对象 包括数组和对象 不能放到依赖中
+  const [param, setParam] = useUrlQueryParam(["name", "personId"]);
 
-  const debouncedValue = useDebounce(param, 1000);
+  const debouncedValue = useDebounce(param, 200);
 
   const { isLoading, error, data: list } = useProjects(debouncedValue);
 
   const { data: users } = useUsers();
 
   useDocumentTitle("项目列表", false);
+
+  //无限渲染问题 怎么去解决 why-did-you-render
+  console.log(useUrlQueryParam(["name"]));
 
   return (
     <Container>
@@ -33,6 +39,7 @@ const ProjectListScreen = () => {
   );
 };
 
+ProjectListScreen.whyDidYouRender = false;
 const Container = styled.div`
   padding: 3.2rem;
 `;
---
+++ src/utils/url.ts
@@ -0,0 +1,59 @@
+import { useMemo } from "react";
+import { URLSearchParamsInit, useSearchParams } from "react-router-dom";
+import { cleanObject } from "utils";
+
+export const useUrlQueryParam = <K extends string>(keys: K[]) => {
+  //URLSearchParams
+  const [searchParams, setSearchParams] = useSearchParams();
+  return [
+    useMemo(
+      () =>
+        keys.reduce((prev, key) => {
+          return { ...prev, [key]: searchParams.get(key) || "" };
+        }, {}) as { [key in K]: string },
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      [searchParams]
+    ),
+    (params: Partial<{ [key in K]: unknown }>) => {
+      //iterator  可以用for..of 进行遍历
+      const o = cleanObject({
+        ...Object.fromEntries(searchParams),
+        ...params,
+      }) as URLSearchParamsInit;
+      return setSearchParams(o);
+    },
+  ] as const;
+};
+
+//怎么让不同的变量拥有同一个类型
+// const a = ['jack', 12, {gender: 'male'}] as const;
+
+//简单iterator 的实现
+/* const obj = {
+    data: ['hello', 'world'],
+    [Symbol.iterator]() {
+      const self = this;
+      let index = 0;
+      console.log(self)
+      return {
+        next() {
+          if(index < self.data.length) {
+            return {
+              value: self.data[index++],
+              done: false
+            }
+          }else{
+            return {
+              value: undefined,
+              done: true
+            }
+          }
+        }
+      }
+    }
+  }
+  
+  for(let o of obj) {
+    console.log(o);
+  }
+ */
